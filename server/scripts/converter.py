d4ec1e729ea39c70ef5da02f864f1d915a415d05
pullundertin <pullundertin@gmail.com>
2023-10-25 08:29:16 +0200

    implemented: collect cwnd data

--- server/scripts/converter.py
+++ server/scripts/converter.py
@@ -6,6 +6,7 @@ pd.options.mode.chained_assignment = None
 input_file = "/shared/tcpprobe/server.log"
 output_file = "/shared/tcpprobe/results.csv"
 output_directory = "/shared/tcpprobe/"
+MSS = 1460
 
 # Lists to store extracted values
 time_values = []
@@ -61,12 +62,35 @@ df = pd.DataFrame(data)
 # Filter communication to server 172.3.0.0/24 only
 combined_df = df[df['src'].str.contains(
     '172.3.') | df['dest'].str.contains('172.3.')]
-client_df = df[df['dest'].str.contains('172.3.')]
-server_df = df[df['src'].str.contains('172.3.')]
+
+# copy send congestion window from server to client dataframe for analysis
+
+
+def get_cwnd():
+
+    client_cwnd_values = []
+    client_cwnd = None
+
+    for host, server_cwnd in zip(combined_df['src'], combined_df['snd_cwnd']):
+        if '172.3.0.5' in host:
+            client_cwnd = server_cwnd * MSS
+            client_cwnd_values.append(None)
+        elif '172.1.0.101' in host:
+            client_cwnd_values.append(client_cwnd)
+
+    # Add 'cwnd' column to the DataFrame
+    combined_df['cwnd'] = client_cwnd_values
+
+
+get_cwnd()
+
+client_df = combined_df[combined_df['dest'].str.contains('172.3.')]
+server_df = combined_df[combined_df['src'].str.contains('172.3.')]
 
 # Normalize the 'time' column to zero
 min_time = min(client_df['time'])
 client_df['time'] -= min_time
+client_df['time'] *= 1000
 
 # data acknowledged by the client can be found in 'data_len'
 client_df['ackd_data'] = client_df['data_len']
@@ -91,9 +115,3 @@ for _, group_df in client_df.groupby(['src', 'dest']):
     src_output_file = f"{output_directory}results_{file_counter}.csv"
     group_df.to_csv(src_output_file, index=False)
     file_counter += 1
-
-# # Save the filtered and normalized DataFrame to a CSV file
-# filtered_df.to_csv(output_file, index=False)
-
-# # Print the DataFrame
-# print(filtered_df)
