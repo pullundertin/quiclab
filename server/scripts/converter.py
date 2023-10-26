c919d7e7ea02bfe8e8af6e0f608c4b9aacf082ce
pullundertin <pullundertin@gmail.com>
2023-10-26 15:38:40 +0200

    refactoring converter & fix server/client side

--- server/scripts/converter.py
+++ server/scripts/converter.py
@@ -1,133 +1,181 @@
 import pandas as pd
 import re
+import numpy as np
 
 pd.options.mode.chained_assignment = None
 
 input_file = "/shared/tcpprobe/server.log"
 output_file = "/shared/tcpprobe/results.csv"
 output_directory = "/shared/tcpprobe/"
+ip_pattern = '172.3.'
 MSS = 1460
 
-# Lists to store extracted values
-time_values = []
-src_values = []
-dest_values = []
-data_len_values = []
-snd_cwnd_values = []
-ssthresh_values = []
-snd_wnd_values = []
-snd_nxt_values = []
-snd_una_values = []
-srtt_values = []
-rcv_wnd_values = []
 
-# Regular expression pattern to match time value, src= and dest=
-pattern = r'(\d+\.\d+): tcp_probe: family=AF_INET src=([^ ]+) dest=([^ ]+) .* data_len=([^ ]+) snd_nxt=([^ ]+) snd_una=([^ ]+) snd_cwnd=([^ ]+) ssthresh=([^ ]+) snd_wnd=([^ ]+) srtt=([^ ]+) rcv_wnd=([^ ]+)'
+def extract_values_from_log(input_file):
+    with open(input_file, 'r') as file:
+        log_data = file.read()
 
-# Read the input file line by line
-with open(input_file, 'r') as file:
-    for line in file:
-        # Use regular expression to find matches in each line
-        matches = re.findall(pattern, line)
-        for match in matches:
-            time_values.append(float(match[0]))
-            src_values.append(match[1])
-            dest_values.append(match[2])
-            data_len_values.append(int(match[3]))
-            snd_nxt_values.append(int(match[4], 16))
-            snd_una_values.append(int(match[5], 16))
-            snd_cwnd_values.append(int(match[6]))
-            ssthresh_values.append(int(match[7]))
-            snd_wnd_values.append(int(match[8]))
-            srtt_values.append(int(match[9]))
-            rcv_wnd_values.append(int(match[10]))
+    pattern = r'(\d+\.\d+): tcp_probe: family=AF_INET src=([^ ]+) dest=([^ ]+) .* data_len=([^ ]+) snd_nxt=([^ ]+) snd_una=([^ ]+) snd_cwnd=([^ ]+) ssthresh=([^ ]+) .* srtt=([^ ]+) rcv_wnd=([^ ]+)'
 
-# Create a DataFrame from the extracted values
-data = {
-    'time': time_values,
-    'src': src_values,
-    'dest': dest_values,
-    'data_len': data_len_values,
-    'snd_nxt': snd_nxt_values,
-    'snd_una': snd_una_values,
-    'snd_cwnd': snd_cwnd_values,
-    'ssthresh': ssthresh_values,
-    'snd_wnd': snd_wnd_values,
-    'srtt': srtt_values,
-    'rcv_wnd': rcv_wnd_values
-}
-df = pd.DataFrame(data)
+    matches = re.findall(pattern, log_data)
+
+    extracted_data = {
+        'time': [float(match[0]) for match in matches],
+        'src': [match[1] for match in matches],
+        'host': [match[2] for match in matches],
+        'data_len': [int(match[3]) for match in matches],
+        'snd_nxt': [int(match[4], 16) for match in matches],
+        'snd_una': [int(match[5], 16) for match in matches],
+        'snd_cwnd': [int(match[6]) for match in matches],
+        'ssthresh': [int(match[7]) for match in matches],
+        'srtt': [int(match[8]) for match in matches],
+        'rcv_wnd': [int(match[9]) for match in matches]
+    }
+
+    return pd.DataFrame(extracted_data)
 
 
-# Filter communication to server 172.3.0.0/24 only
-combined_df = df[df['src'].str.contains(
-    '172.3.') | df['dest'].str.contains('172.3.')]
-
-# copy send congestion window from server to client dataframe for analysis
+def filter_communication_with_device_under_test(dataframe):
+    return dataframe[dataframe['src'].str.contains(ip_pattern) | dataframe['host'].str.contains(ip_pattern)]
 
 
-def get_cwnd():
+def convert_microseconds_to_timestamp(dataframe):
+    # Convert the 'time' column to pandas datetime object
+    # dataframe['time'] = dataframe['time']
+    dataframe['time'] = dataframe['time'] * 2000
+    dataframe['time'] = pd.to_datetime(dataframe['time'], unit='s')
 
-    client_cwnd_values = []
-    client_cwnd = 0
-
-    for host, server_cwnd in zip(combined_df['src'], combined_df['snd_cwnd']):
-        if '172.3.0.5' in host:
-            client_cwnd = server_cwnd * MSS
-            client_cwnd_values.append(0)
-        elif '172.1.0.101' in host:
-            client_cwnd_values.append(client_cwnd)
-
-    # Add 'cwnd' column to the DataFrame
-    combined_df['cwnd'] = client_cwnd_values
-    # combined_df['cwnd'] = combined_df['cwnd'].astype(int)
-    # print(combined_df)
+    return dataframe
 
 
-def convert_timestamp():
-    # Convert the 'timestamp' column to pandas datetime object
-    combined_df['time'] = pd.to_datetime(combined_df['time'], unit='s')
-
-    # Extract milliseconds part from the timestamp
-    combined_df['milliseconds'] = combined_df['time'].apply(
-        lambda x: int((x.microsecond / 1000)))
-
-    # Create a timedelta object with milliseconds
-    combined_df['milliseconds_delta'] = combined_df['milliseconds'].apply(
-        lambda x: pd.Timedelta(milliseconds=x))
-
-    # Get the current time
-    current_time = pd.to_datetime('now')
-
-    # Add milliseconds to the current time
-    combined_df['timestamp'] = current_time + combined_df['milliseconds_delta']
+def filter_data_sent_by_client(dataframe):
+    return dataframe[dataframe['src'].str.contains(ip_pattern)]
 
 
-get_cwnd()
-convert_timestamp()
-
-client_df = combined_df[combined_df['dest'].str.contains('172.3.')]
-server_df = combined_df[combined_df['src'].str.contains('172.3.')]
+def filter_data_sent_by_server(dataframe):
+    return dataframe[dataframe['host'].str.contains(ip_pattern)]
 
 
-# data acknowledged by the client can be found in 'data_len'
-client_df['ackd_data'] = client_df['data_len']
+def create_congestion_window_column(dataframe):
+    client_cwnd = {ip_pattern: 0}
 
-# Group by 'src' and 'dest', then calculate cumulative sum of 'ackd_data'
-client_df['cum_ackd_data'] = client_df.groupby(['src', 'dest'])[
-    'ackd_data'].cumsum()
+    def calculate_client_cwnd(row):
+        nonlocal client_cwnd
+        if ip_pattern in row['src']:
+            client_cwnd[ip_pattern] = row['snd_cwnd'] * MSS
+            return 0
+        else:
+            return client_cwnd[ip_pattern]
+
+    dataframe['cwnd'] = dataframe.apply(calculate_client_cwnd, axis=1)
+
+    # Remove temporary columns
+    dataframe.drop(
+        columns=['snd_cwnd'], inplace=True)
+
+    return dataframe
 
 
-# Calculate the difference between 'snd_nxt' and 'snd_una' and add it as a new column
-client_df['cum_rcv'] = client_df['cum_ackd_data'] + client_df['rcv_wnd']
+def update_rcv_wnd_values_for_server(dataframe):
+
+    # Iterate through the DataFrame and update rcv_wnd values for src 172.3.0.5 based on src 172.1.0.101
+    last_known_rcv_wnd = None
+    for index, row in dataframe.iterrows():
+        if ip_pattern not in row['src']:
+            last_known_rcv_wnd = row['rcv_wnd']
+        elif ip_pattern in row['src'] and last_known_rcv_wnd is not None:
+            dataframe.at[index, 'rcv_wnd'] = last_known_rcv_wnd
+
+    return dataframe
 
 
-# Group by 'src' and 'dest', then calculate cumulative sum of 'data_len'
-client_df['cum_data_len'] = client_df.groupby(['src', 'dest'])[
-    'data_len'].cumsum()
+def create_cumulated_receive_window_column(client_dataframe):
+    # Calculate the difference between 'snd_nxt' and 'snd_una' and add it as a new column
+    client_dataframe['cum_rcv_wnd'] = client_dataframe['cum_ack_sent'] + \
+        client_dataframe['rcv_wnd']
+    return client_dataframe
 
-# get minimum of rcv_wnd and cwnd
-client_df['min_wnd'] = client_df[['rcv_wnd', 'cwnd']].min(axis=1)
 
-# export dataframe to csv
-client_df.to_csv(output_file, index=False)
+def create_cumulated_bytes_in_flight_column(dataframe):
+    bytes_in_flight = {ip_pattern: 0}
+
+    def calculate_bytes_in_flight_by_server(row):
+        nonlocal bytes_in_flight
+        if ip_pattern in row['src']:
+            bytes_in_flight[ip_pattern] = row['snd_nxt'] - row['snd_una']
+            return 0
+        else:
+            return bytes_in_flight[ip_pattern]
+
+    dataframe['bytes_in_flight'] = dataframe.apply(
+        calculate_bytes_in_flight_by_server, axis=1)
+
+    # # Remove temporary columns
+    # dataframe.drop(
+    #     columns=['snd_cwnd'], inplace=True)
+
+    dataframe['cum_bytes_in_flight'] = dataframe.groupby(['src', 'host'])[
+        'bytes_in_flight'].cumsum()
+    return dataframe
+
+
+def create_minimum_of_cwnd_and_rcv_wnd_column(dataframe):
+    dataframe['min_wnd'] = dataframe[['rcv_wnd', 'cwnd']].min(axis=1)
+    return dataframe
+
+
+def export_to_csv(dataframe, output_file):
+    dataframe.to_csv(output_file, index=False)
+
+
+def data_sent_by_server(dataframe):
+    # Rename 'data_len' column
+    dataframe['data_sent'] = dataframe['data_len']
+    # Delete rows where 'data_sent' is equal to 0
+    dataframe = dataframe[dataframe['data_sent'] != 0]
+
+    # Calculate cumulated data sent
+    dataframe['cum_data_sent'] = dataframe.groupby(['src', 'host'])[
+        'data_sent'].cumsum()
+    return dataframe
+
+
+def acks_sent_by_client(dataframe):
+
+    # Calculate the acknowledgements as difference between snd_una and the succeeding snd_una value
+    dataframe['ack_sent'] = dataframe['snd_una'].shift(
+        -1) - dataframe['snd_una']
+
+    # Remove NaN values in the 'ack_sent' column (last row where there is no succeeding row)
+    dataframe = dataframe.dropna(subset=['ack_sent'])
+
+    # Delete rows where 'ack_sent' is equal to 0
+    dataframe = dataframe[dataframe['ack_sent'] != 0]
+
+    # Calculate cumulated acknowledgements
+    dataframe['cum_ack_sent'] = dataframe.groupby(['src', 'host'])[
+        'ack_sent'].cumsum()
+    return dataframe
+
+
+if __name__ == "__main__":
+    dataframe = extract_values_from_log(input_file)
+    dataframe = convert_microseconds_to_timestamp(dataframe)
+    dataframe = filter_communication_with_device_under_test(dataframe)
+    dataframe = update_rcv_wnd_values_for_server(dataframe)
+    dataframe = create_congestion_window_column(dataframe)
+    export_to_csv(
+        dataframe[['src', 'rcv_wnd', 'cwnd']], '/shared/tcpprobe/test.csv')
+    server_dataframe = filter_data_sent_by_server(dataframe)
+    client_dataframe = filter_data_sent_by_client(dataframe)
+    server_dataframe = data_sent_by_server(server_dataframe)
+    client_dataframe = acks_sent_by_client(client_dataframe)
+    client_dataframe = create_cumulated_receive_window_column(
+        client_dataframe)
+
+    server_selection = server_dataframe[[
+        'time', 'data_sent', 'cum_data_sent', 'cwnd']]
+    client_selection = client_dataframe[[
+        'time', 'src', 'snd_una', 'snd_nxt', 'ack_sent', 'cum_ack_sent', 'rcv_wnd', 'cum_rcv_wnd']]
+    export_to_csv(server_selection, '/shared/tcpprobe/server.csv')
+    export_to_csv(client_selection, '/shared/tcpprobe/client.csv')
