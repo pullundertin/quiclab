c546b30d4202fa7d49c845fecd2482b9705731fd
pullundertin <pullundertin@gmail.com>
2023-10-29 00:32:45 +0200

    refactoring

--- client/scripts/run_client.py
+++ client/scripts/run_client.py
@@ -1,22 +1,13 @@
 
 import psutil
 import time
-import socket
 import os
 import subprocess
-import concurrent.futures
 from concurrent.futures import ThreadPoolExecutor
 from concurrent.futures import wait
-import sys
 import logging
 import argparse
 
-# Get environment variables
-KEYS_PATH = os.getenv("KEYS_PATH")
-QLOG_PATH = os.getenv("QLOG_PATH")
-TICKET_PATH = os.getenv("TICKET_PATH")
-PCAP_PATH = os.getenv("PCAP_PATH")
-HOST = os.getenv('HOST')
 
 # Configure logging
 logging.basicConfig(filename='/shared/logs/output.log', level=logging.INFO,
@@ -40,109 +31,73 @@ def arguments():
     parser.add_argument('--rmax', type=str,
                         help='maximum recieve window in bytes')
 
-    # Parse the command-line arguments
-    global args
     args = parser.parse_args()
 
-    # Access the flag value in your script
+    logging.info(f"{os.getenv('HOST')}: {args.mode} mode enabled")
 
-    logging.info(f'{HOST}: {args.mode} mode enabled')
-
-
-def map_function():
-    # Create a dictionary that maps string keys to functions
-    function_mapping = {
-        "http": http,
-        "aioquic": aioquic,
-        "quicgo": quicgo
-    }
-
-    # Call the chosen function based on the string variable
-    if args.mode in function_mapping:
-        function_call = function_mapping[args.mode]
-        function_call()
-    else:
-        logging.info("Function not found.")
-
-
-def initialize():
-    arguments()
+    return args
 
 
 def run_command(command):
     try:
         process = subprocess.run(
             command, check=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        logging.info(f"{HOST}: {process.stdout.decode()}")
+        logging.info(f"{os.getenv('HOST')}: {process.stdout.decode()}")
     except subprocess.CalledProcessError as e:
-        logging.info(f"Error on {HOST}: {e}")
-        logging.info(f"Error {HOST} output: {e.stderr.decode()}")
+        logging.info(f"Error on {os.getenv('HOST')}: {e}")
+        logging.info(f"Error {os.getenv('HOST')} output: {e.stderr.decode()}")
 
 
-def tcp_settings():
-    command = f'sysctl -w net.ipv4.tcp_window_scaling={args.window_scaling}'
+def tcp_settings(args):
+    command = f"sysctl -w net.ipv4.tcp_window_scaling={args.window_scaling}"
     run_command(command)
-    command = f'sysctl -w net.ipv4.tcp_rmem="{args.rmin} {args.rdef} {args.rmax}"'
+    command = f"sysctl -w net.ipv4.tcp_rmem='{args.rmin} {args.rdef} {args.rmax}'"
     run_command(command)
 
 
 def tcpdump():
-
-    # Command to run
-    command = "tcpdump -i eth0 -w $PCAP_PATH"
-
-    logging.info(
-        f"{HOST}: tcpdump started.")
+    command = "tcpdump -i eth0 -w $PCAP_PATH -n"
+    logging.info(f"{os.getenv('HOST')}: tcpdump started.")
     run_command(command)
 
 
 def aioquic():
-
-    IP = "172.3.0.5"
-    PORT = 4433
-
-    # Command to run
-    command = "python /aioquic/examples/http3_client.py -k https://172.3.0.5:4433/data.log https://172.3.0.5:4433/data.log --secrets-log $KEYS_PATH --quic-log $QLOG_PATH --zero-rtt --session-ticket $TICKET_PATH"
-
-    logging.info(f"{HOST}: sending aioquic request...")
+    URL = "https://172.3.0.5:4433/data.log"
+    command = f"python /aioquic/examples/http3_client.py -k {URL} {URL} --secrets-log $KEYS_PATH --quic-log $QLOG_PATH --zero-rtt --session-ticket $TICKET_PATH"
+    logging.info(f"{os.getenv('HOST')}: sending aioquic request...")
     run_command(command)
 
 
 def quicgo():
-
-    IP = "172.3.0.5"
-    PORT = 6121
-
-    # Change current working directory
+    URL = "https://172.3.0.5:6121/data.log"
     os.chdir("/quic-go/example/client")
-
-    # Command to run
-    command = f'go run main.go --insecure --keylog {KEYS_PATH} --qlog https://{IP}:{PORT}/data.log https://{IP}:{PORT}/data.log'
-
-    logging.info(f"{HOST}: sending quic-go request...")
+    command = f"go run main.go --insecure --keylog $KEYS_PATH --qlog {URL} {URL}"
+    logging.info(f"{os.getenv('HOST')}: sending quic-go request...")
     run_command(command)
 
 
-def http():
-
-    tcp_settings()
-
-    IP = '172.3.0.5'
-    PORT = 443
-
-    # Set SSLKEYLOGFILE
-    os.environ['SSLKEYLOGFILE'] = KEYS_PATH
-
-    # Command to run
-    command = "curl -k https://172.3.0.5:443/data.log -o /dev/null https://172.3.0.5:443/data.log -o/dev/null"
-
-    logging.info(f"{HOST}: sending http request...")
+def http(args):
+    tcp_settings(args)
+    URL = "https://172.3.0.5:443/data.log"
+    os.environ['SSLKEYLOGFILE'] = os.getenv("KEYS_PATH")
+    command = f"curl -k {URL} -o /dev/null {URL} -o/dev/null"
+    logging.info(f"{os.getenv('HOST')}: sending http request...")
     run_command(command)
+
+
+def client_request(args):
+
+    if args.mode == "http":
+        http(args)
+    elif args.mode == "aioquic":
+        aioquic()
+    elif args.mode == "quicgo":
+        quicgo()
 
 
 def kill(process_name):
 
-    for process in psutil.process_iter(attrs=['pid', 'ppid', 'name']):
+    for process in psutil.process_iter(attrs=['pid', 'name']):
         if process.info['name'] == process_name:
 
             try:
@@ -157,16 +112,16 @@ def kill(process_name):
 
 if __name__ == "__main__":
 
-    initialize()
+    args = arguments()
 
     with ThreadPoolExecutor() as executor:
 
         thread_1 = executor.submit(tcpdump)
         time.sleep(3)
-        thread_2 = executor.submit(map_function)
+        thread_2 = executor.submit(client_request, args)
 
         wait([thread_2])
-        logging.info(f'{HOST}: request completed.')
+        logging.info(f"{os.getenv('HOST')}: request completed.")
 
         time.sleep(3)
         kill("tcpdump")
