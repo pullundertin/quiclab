aee7f4965ccaf59c6707e5a79a257c4d2477c773
pullundertin <pullundertin@gmail.com>
2023-10-28 20:32:22 +0200

    refactoring: build setup files

--- build/quiclab/setup.sh
+++ build/quiclab/setup.sh
@@ -1,57 +1,67 @@
 #!/bin/bash
 
-# Update ubuntu
-apt update && 
-apt upgrade -y &&
+# Log file path
+LOG_FILE="/setup.log"
+
+# Function to log messages
+log_message() {
+    local message="$1"
+    echo "$(date +"%Y-%m-%d %T"): $message"
+    echo "$(date +"%Y-%m-%d %T"): $message" >> "$LOG_FILE"
+}
+
+# Function to run a command and log its output
+run_and_log_command() {
+    local command="$1"
+    log_message "Running command: $command"
+    echo "Output of command: $command" >> "$LOG_FILE"
+    eval "$command" 2>&1 | tee -a "$LOG_FILE"
+}
+
+# Update Ubuntu
+run_and_log_command "apt update"
+log_message "Ubuntu update complete."
+
+# Upgrade packages
+run_and_log_command "apt upgrade -y"
+log_message "Package upgrade complete."
 
 # Install dependencies
-apt install git vim python-is-python3 pip curl iputils-ping net-tools bc iproute2 libssl-dev python3-dev nginx tcpdump wget iptables -y &&
-# apt install DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata &&
+run_and_log_command "apt install git vim python-is-python3 pip curl iputils-ping net-tools bc iproute2 libssl-dev python3-dev nginx tcpdump wget python3-psutil iptables -y"
+log_message "Dependency installation complete."
 
 # Install aioquic
-git clone https://github.com/aiortc/aioquic.git &&
-pip install aioquic &&
-cd /aioquic &&
-pip install --upgrade pip setuptools &&
-pip install -e . &&
-pip install asgiref dnslib "flask<2.2" httpbin starlette "werkzeug<2.1" wsproto &&
+run_and_log_command "git clone https://github.com/aiortc/aioquic.git"
+run_and_log_command "pip install -e ./aioquic"
+log_message "aioquic installation complete."
 
 # Prepare nginx
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /example.key -out /example.crt -subj "/C=DE/ST=Berlin/L=GERMANY/O=Dis/CN=www.example.com" &&
-ln -s /data/data.log /var/www/html/data.log &&
+run_and_log_command "openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /example.key -out /example.crt -subj \"/C=DE/ST=Berlin/L=GERMANY/O=Dis/CN=www.example.com\""
+run_and_log_command "ln -s /data/data.log /var/www/html/data.log"
+log_message "Nginx preparation complete."
 
 # Install quic-go
-cd / &&
-git clone https://github.com/quic-go/quic-go.git &&
-
-# install go
-cd / &&
-
+run_and_log_command "git clone https://github.com/quic-go/quic-go.git"
 architecture=$(uname -m)
-
 if [ "$architecture" == "x86_64" ]; then
-    wget https://go.dev/dl/go1.21.3.linux-amd64.tar.gz &&
-    rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.3.linux-amd64.tar.gz
+    run_and_log_command "wget https://go.dev/dl/go1.21.3.linux-amd64.tar.gz"
+    run_and_log_command "rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.3.linux-amd64.tar.gz"
 elif [ "$architecture" == "aarch64" ]; then
-    wget https://go.dev/dl/go1.21.3.linux-arm64.tar.gz
-    rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.3.linux-arm64.tar.gz
+    run_and_log_command "wget https://go.dev/dl/go1.21.3.linux-arm64.tar.gz"
+    run_and_log_command "rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.3.linux-arm64.tar.gz"
 else
-    echo "The architecture is not recognized: $architecture"
+    log_message "The architecture is not recognized: $architecture"
 fi
-
-ln -s /usr/local/go/bin/go /usr/bin/go &&
-
-cd /quic-go &&
-go get -d ./... &&
+run_and_log_command "ln -s /usr/local/go/bin/go /usr/bin/go"
+run_and_log_command "cd /quic-go && go get -d ./..."
+log_message "quic-go installation complete."
 
 # Install pandas
-pip install pandas &&
+run_and_log_command "pip install pandas"
+log_message "Pandas installation complete."
 
-# Install docker
-pip install docker &&
+# Install Docker
+run_and_log_command "pip install docker"
+log_message "Docker installation complete."
 
-# Install psutil
-apt install python3-psutil
-
-
-
+log_message "Script execution complete."
--- build/qvis/setup.sh
+++ build/qvis/setup.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
-# Update ubuntu
-apt update && 
-apt upgrade -y &&
+# Log file path
+LOG_FILE="/setup.log"
 
-# Install dependencies
-apt install git vim npm -y &&
+# Function to log messages
+log_message() {
+    local message="$1"
+    echo "$(date +"%Y-%m-%d %T"): $message"
+    echo "$(date +"%Y-%m-%d %T"): $message" >> "$LOG_FILE"
+}
 
-# Install qvis
-git clone https://github.com/quiclog/qvis.git &&
-cd /qvis/visualizations/ &&
+# Function to run a command and log its output
+run_and_log_command() {
+    local command="$1"
+    log_message "Running command: $command"
+    echo "Output of command: $command" >> "$LOG_FILE"
+    eval "$command" 2>&1 | tee -a "$LOG_FILE"
+}
 
-# Install npm
-npm install
+# Update Ubuntu 
+run_and_log_command "apt update && apt upgrade -y"
 
+# Install dependencies 
+run_and_log_command "apt install git vim npm -y"
 
+# Clone qvis repository and install npm packages
+run_and_log_command "git clone https://github.com/quiclog/qvis.git"
+run_and_log_command "cd /qvis/visualizations/ && npm install"
 
-
-
-
-
+log_message "Script execution complete."
