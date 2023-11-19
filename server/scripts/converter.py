d50b3152905175e69c109a13ee1af36f7be3db12
pullundertin <pullundertin@gmail.com>
2023-11-19 09:48:33 +0100

    refactoring mss and bytes

--- server/scripts/converter.py
+++ server/scripts/converter.py
@@ -18,9 +18,9 @@ def extract_values_from_log(input_file):
         'snd_nxt': [int(match[4], 16) for match in matches],
         'snd_una': [int(match[5], 16) for match in matches],
         'snd_cwnd': [int(match[6]) for match in matches],
-        'ssthresh_org': [int(match[7]) for match in matches],
+        'ssthresh': [int(match[7]) for match in matches],
         'srtt': [int(match[8]) for match in matches],
-        'rcv_wnd_org': [int(match[9]) for match in matches]
+        'rcv_wnd': [int(match[9]) for match in matches]
     }
 
     return pd.DataFrame(extracted_data)
@@ -45,7 +45,7 @@ def create_congestion_window_column(dataframe):
             return None
 
     # Apply the function to create 'data_sent' column
-    dataframe['cwnd'] = dataframe.apply(copy_cwnd, axis=1)
+    dataframe['cwnd_mss'] = dataframe.apply(copy_cwnd, axis=1)
 
     return dataframe
 
@@ -53,36 +53,36 @@ def create_congestion_window_column(dataframe):
 def create_ssthresh_column(dataframe):
     def copy_ssthresh(row):
         if row['src'].startswith('172.3.'):
-            return row['ssthresh_org']
+            return row['ssthresh']
         else:
             return None
 
     # Apply the function to create 'data_sent' column
-    dataframe['ssthresh'] = dataframe.apply(copy_ssthresh, axis=1)
+    dataframe['ssthresh_mss'] = dataframe.apply(copy_ssthresh, axis=1)
 
     return dataframe
 
 
 def data_sent_by_server(dataframe):
-    # Function to copy 'data_len' values to 'data_sent' for rows where 'src' starts with '172.1.'
+    # Function to copy 'data_len' values to 'data_sent_bytes' for rows where 'src' starts with '172.1.'
     def copy_data_sent(row):
         if row['src'].startswith('172.1.'):
             return row['data_len']
         else:
             return None
 
-    # Apply the function to create 'data_sent' column
-    dataframe['data_sent'] = dataframe.apply(copy_data_sent, axis=1)
+    # Apply the function to create 'data_sent_bytes' column
+    dataframe['data_sent_bytes'] = dataframe.apply(copy_data_sent, axis=1)
 
     # Calculate cumulated data sent
-    dataframe['cum_data_sent'] = dataframe.groupby(['src', 'dest'])[
-        'data_sent'].cumsum()
+    dataframe['cum_data_sent_bytes'] = dataframe.groupby(['src', 'dest'])[
+        'data_sent_bytes'].cumsum()
     return dataframe
 
 
 def acks_sent_by_client(dataframe):
     # Create a new column 'ack_sent' and fill it with None
-    dataframe['ack_sent'] = None
+    dataframe['ack_sent_bytes'] = None
     # Get indices of rows with src starting with '172.3'
     src_indices = dataframe[dataframe['src'].str.startswith(
         '172.3')].index.tolist()
@@ -93,13 +93,13 @@ def acks_sent_by_client(dataframe):
 
         snd_una_diff = dataframe.loc[next_index, 'snd_una'] - \
             dataframe.loc[current_index, 'snd_una']
-        dataframe.at[current_index, 'ack_sent'] = snd_una_diff
+        dataframe.at[current_index, 'ack_sent_bytes'] = snd_una_diff
 
     # Calculate cumulated acknowledgements
-    # Convert 'ack_sent' column to float for calculations
-    dataframe['ack_sent'] = dataframe['ack_sent'].astype(float)
-    dataframe['cum_ack_sent'] = dataframe['ack_sent'].astype(
-        float).cumsum()  # Create 'cum_ack_sent' column with cumulative sum
+    # Convert 'ack_sent_bytes' column to float for calculations
+    dataframe['ack_sent_bytes'] = dataframe['ack_sent_bytes'].astype(float)
+    dataframe['cum_ack_sent_bytes'] = dataframe['ack_sent_bytes'].astype(
+        float).cumsum()  # Create 'cum_ack_sent_bytes' column with cumulative sum
 
     return dataframe
 
@@ -107,46 +107,47 @@ def acks_sent_by_client(dataframe):
 def create_client_rcv_wnd_column(dataframe):
     def copy_rcv_wnd(row):
         if row['src'].startswith('172.1.'):
-            return row['rcv_wnd_org']
+            return row['rcv_wnd']
         else:
             return None
 
     # Apply the function to create 'data_sent' column
-    dataframe['rcv_wnd'] = dataframe.apply(copy_rcv_wnd, axis=1)
+    dataframe['rcv_wnd_bytes'] = dataframe.apply(copy_rcv_wnd, axis=1)
     return dataframe
 
 
 def fill_NaN_values_with_last_known_values(dataframe):
-    columns_to_fill = ['cwnd', 'ssthresh', 'rcv_wnd',
-                       'cum_data_sent', 'cum_ack_sent']
+    columns_to_fill = ['cwnd_mss', 'ssthresh_mss', 'rcv_wnd_bytes',
+                       'cum_data_sent_bytes', 'cum_ack_sent_bytes']
     dataframe[columns_to_fill] = dataframe[columns_to_fill].fillna(
         method='ffill')
     return dataframe
 
 
 def create_cum_rcv_wnd_column(dataframe):
-    dataframe['cum_rcv_wnd'] = dataframe['cum_ack_sent'] + \
-        dataframe['rcv_wnd']
+    dataframe['cum_rcv_wnd_bytes'] = dataframe['cum_ack_sent_bytes'] + \
+        dataframe['rcv_wnd_bytes']
 
     return dataframe
 
 
 def create_minimum_of_cwnd_and_rcv_wnd_column(dataframe):
-    dataframe['rcv_wnd_mss'] = round(dataframe['rcv_wnd'] / MSS, 1)
-    dataframe['cwnd_bytes'] = dataframe['cwnd'] * MSS
-    dataframe['min_wnd'] = dataframe[['rcv_wnd', 'cwnd_bytes']].min(axis=1)
-    dataframe['min_wnd_mss'] = dataframe[['rcv_wnd_mss', 'cwnd']].min(axis=1)
+    dataframe['rcv_wnd_mss'] = round(dataframe['rcv_wnd_bytes'] / MSS, 1)
+    dataframe['cwnd_bytes'] = dataframe['cwnd_mss'] * MSS
+    dataframe['min_wnd_bytes'] = dataframe[[
+        'rcv_wnd_bytes', 'cwnd_bytes']].min(axis=1)
+    dataframe['min_wnd_mss'] = dataframe[[
+        'rcv_wnd_mss', 'cwnd_mss']].min(axis=1)
     return dataframe
 
 
 def hide_ssthresh_values_greater_than_max_wnd_size(dataframe):
     def maximum_of_cwnd_and_rcv_wnd(dataframe):
-        max = dataframe[['rcv_wnd_mss', 'cwnd']].max().max()
-        print(max)
-        return max
+        return dataframe[['rcv_wnd_mss', 'cwnd_mss']].max().max()
 
     def delete_values_exceeding_threshold(dataframe, threshold):
-        dataframe.loc[dataframe['ssthresh'] > threshold, 'ssthresh'] = pd.NA
+        dataframe.loc[dataframe['ssthresh_mss']
+                      > threshold, 'ssthresh_mss'] = pd.NA
         return dataframe
 
     threshold = maximum_of_cwnd_and_rcv_wnd(dataframe)
@@ -207,7 +208,7 @@ if __name__ == "__main__":
     dataframe = create_minimum_of_cwnd_and_rcv_wnd_column(dataframe)
     dataframe = hide_ssthresh_values_greater_than_max_wnd_size(dataframe)
     # dataframe = create_rtt_graph(dataframe)
-    selected_columns = ['time', 'cwnd_bytes', 'cwnd', 'rcv_wnd_mss', 'min_wnd_mss', 'ssthresh', 'rcv_wnd', 'cum_rcv_wnd',
-                        'data_sent', 'cum_data_sent', 'ack_sent', 'cum_ack_sent']
+    selected_columns = ['time', 'cwnd_bytes', 'rcv_wnd_bytes', 'min_wnd_bytes', 'cwnd_mss', 'rcv_wnd_mss', 'min_wnd_mss', 'ssthresh_mss',  'cum_rcv_wnd_bytes',
+                        'data_sent_bytes', 'cum_data_sent_bytes', 'ack_sent_bytes', 'cum_ack_sent_bytes']
 
     export_to_csv(dataframe, output_file, selected_columns)
