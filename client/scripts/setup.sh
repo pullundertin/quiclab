e5f4e95c381f112e88968f6b0fa17f90392c5d4e
pullundertin <pullundertin@gmail.com>
2023-11-22 14:12:56 +0100

    disable TCP Fast Open

--- client/scripts/setup.sh
+++ client/scripts/setup.sh
@@ -4,3 +4,7 @@
 route add -net 172.2.0.0/24 gw 172.1.0.3 eth0
 route add -net 172.3.0.0/24 gw 172.1.0.3 eth0
 
+# disable TCP Fast Open
+sysctl -w net.ipv4.tcp_fastopen=0
+
+
--- router/scripts/setup.sh
+++ router/scripts/setup.sh
@@ -3,3 +3,7 @@
 route add -net $SUBNET_1 gw $GW_1 $IFACE 
 route add -net $SUBNET_2 gw $GW_2 $IFACE 
 
+
+# disable TCP Fast Open
+sysctl -w net.ipv4.tcp_fastopen=0
+
