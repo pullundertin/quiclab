37962c720c7049ed7f011827f525596a8846abad
pullundertin <pullundertin@gmail.com>
2023-12-10 17:32:37 +0100

    fixed: tcp hs including tls

--- modules/pcap_processing.py
+++ modules/pcap_processing.py
@@ -24,104 +24,70 @@ def convert_pcap_to_json():
 
 
 def read_json_files(file_path):
-    # Open the JSON file in read mode
     with open(file_path, 'r') as file:
-        # Load the JSON data into a Python dictionary
         return json.load(file)
 
 
 def get_tcp_handshake_time():
     for filename in os.listdir(PCAP_PATH):
-        if filename.endswith('.json'):
+        if filename.endswith('client_1.pcap.json'):
             json_file = os.path.join(PCAP_PATH, filename)
 
             json_data = read_json_files(json_file)
             for packet in json_data:
                 if 'tls' in packet["_source"]["layers"] and 'tls.record' in packet["_source"]["layers"]["tls"] and 'tls.handshake' in packet["_source"]["layers"]["tls"]["tls.record"]:
                     tls_handshake = packet["_source"]["layers"]["tls"]["tls.record"]["tls.handshake"]
+                    ip_source = packet["_source"]["layers"]["ip"]["ip.src"]
                     if 'tls.handshake.type' in tls_handshake:
                         handshake_type = tls_handshake['tls.handshake.type']
                         time = float(packet["_source"]["layers"]
                                      ["frame"]["frame.time_relative"])
-                        if handshake_type == '20':
+                        if handshake_type == '20' and ip_source == "172.1.0.101":
                             tcp_handshake_durations.append(time)
-            # # Initialize variables to store relevant data
-            # syn_ack_packet = None
-            # tcp_handshake_duration = None
-
-            # # Loop through each packet in the JSON data
-            # for packet in json_data:
-            #     if 'tcp' in packet.get('_source', {}).get('layers', {}):
-            #         tcp_flags = packet["_source"]["layers"]["tcp"]["tcp.flags_tree"]
-
-            #         # Check if the packet has both SYN and ACK flags set
-            #         if tcp_flags.get("tcp.flags.syn") == "1" and tcp_flags.get("tcp.flags.ack") == "1":
-            #             syn_ack_packet = packet
-            #             break  # Exit the loop after finding the SYN+ACK packet
-
-            # # If a SYN+ACK packet is found, search for the corresponding packet with matching sequence and acknowledgment numbers
-            # if syn_ack_packet:
-            #     syn_ack_seq_ack = syn_ack_packet["_source"]["layers"]["tcp"]["tcp.ack_raw"]
-            #     for packet in json_data:
-            #         seq_ack = packet["_source"]["layers"]["tcp"]["tcp.seq_raw"]
-
-            #         # Check if the sequence and acknowledgment numbers match
-            #         if seq_ack == syn_ack_seq_ack:
-            #             tcp_handshake_duration = float(
-            #                 packet["_source"]["layers"]["frame"]["frame.time_relative"])
-            #             break  # Exit the loop after finding the matching packet
-
-            # # Print the frame.time_relative of the matching packet (if found)
-
     return tcp_handshake_durations
 
 
 def get_tcp_connection_time():
+    tcp_connection_durations = []
+
     for filename in os.listdir(PCAP_PATH):
-        if filename.endswith('.json'):
+        if filename.endswith('client_1.pcap.json'):
             json_file = os.path.join(PCAP_PATH, filename)
 
             json_data = read_json_files(json_file)
-            # Initialize variables to store relevant data
-            fin_packet = None
-            tcp_connection_duration = None
+            fin_ack_seq = None
 
-            # Loop through each packet in the JSON data to find the server's FIN packet
+            # Find the server's FIN packet
             for packet in json_data:
                 if 'tcp' in packet.get('_source', {}).get('layers', {}):
                     ip = packet["_source"]["layers"]["ip"]
                     tcp = packet["_source"]["layers"]["tcp"]
                     tcp_flags = tcp["tcp.flags_tree"]
 
-                    # Check for a packet sent by the server with IP 172.3.0.5 with FIN flag set
                     if ip["ip.src"] == "172.3.0.5" and tcp_flags.get("tcp.flags.fin") == "1":
-                        fin_packet = packet
                         fin_ack_seq = tcp["tcp.ack_raw"]
-                        break  # Exit loop after finding the server's FIN packet
+                        break
 
-            # If the server's FIN packet is found, search for the client's packet with matching sequence number
-            if fin_packet:
+            # Find the client's packet with the matching sequence number
+            if fin_ack_seq:
                 for packet in json_data:
-                    ip = packet["_source"]["layers"]["ip"]
-                    tcp = packet["_source"]["layers"]["tcp"]
-                    tcp_flags = tcp["tcp.flags_tree"]
+                    if 'ip' in packet["_source"]["layers"]:
+                        ip = packet["_source"]["layers"]["ip"]
+                        tcp = packet["_source"]["layers"]["tcp"]
 
-                    # Check for a packet sent by the client with IP 172.1.0.101 with matching sequence number
-                    if ip["ip.src"] == "172.1.0.101" and tcp.get("tcp.seq_raw") == fin_ack_seq:
-                        tcp_connection_duration = float(
-                            packet["_source"]["layers"]["frame"]["frame.time_relative"])
-                        break  # Exit loop after finding the matching ACK packet
-
-            # Print the frame.time_relative of the matching packet (if found)
-            if tcp_connection_duration:
-                tcp_connection_durations.append(tcp_connection_duration)
+                        if ip["ip.src"] == "172.1.0.101" and tcp.get("tcp.seq_raw") == fin_ack_seq:
+                            tcp_connection_duration = float(
+                                packet["_source"]["layers"]["frame"]["frame.time_relative"])
+                            tcp_connection_durations.append(
+                                tcp_connection_duration)
+                            break
 
     return tcp_connection_durations
 
 
 def get_quic_connection_time():
     for filename in os.listdir(PCAP_PATH):
-        if filename.endswith('.json'):
+        if filename.endswith('client_1.pcap.json'):
             json_file = os.path.join(PCAP_PATH, filename)
 
             json_data = read_json_files(json_file)
@@ -140,7 +106,7 @@ def get_quic_connection_time():
 
 def get_quic_handshake_time():
     for filename in os.listdir(PCAP_PATH):
-        if filename.endswith('.json'):
+        if filename.endswith('client_1.pcap.json'):
             json_file = os.path.join(PCAP_PATH, filename)
 
             json_data = read_json_files(json_file)
@@ -199,3 +165,11 @@ def get_statistics():
     calculate_and_log_stats(tcp_connection_durations, 'tcp_conn')
     calculate_and_log_stats(quic_handshake_durations, 'quic_hs')
     calculate_and_log_stats(quic_connection_durations, 'quic_conn')
+    for i in tcp_handshake_durations:
+        print('hs', i)
+    for i in tcp_connection_durations:
+        print('conn', i)
+    for i in quic_handshake_durations:
+        print(i)
+    for i in quic_connection_durations:
+        print(i)
