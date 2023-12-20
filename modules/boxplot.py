c56c43b8fa071e01e1c52795753365f6074c7cbd
pullundertin <pullundertin@gmail.com>
2023-12-20 16:02:34 +0100

    implemented boxplots for every value of independent variable

--- modules/boxplot.py
+++ modules/boxplot.py
@@ -9,10 +9,13 @@ from modules.prerequisites import read_configuration
 
 BOXPLOTS_DIR = read_configuration().get("BOXPLOTS_DIR")
 
-def combine_quic_and_tcp_values_for(df, column_postfix):
+
+def combine_quic_and_tcp_values_for(df, column_postfix, value):
     """Combine QUIC and TCP values into a single column."""
-    melted_df = pd.melt(df, id_vars=['mode'], value_vars=[f'quic_{column_postfix}', f'tcp_{column_postfix}'], var_name='Protocol', value_name=f'time_{column_postfix}')
+    melted_df = pd.melt(df, id_vars=['mode'], value_vars=[
+                        f'quic_{column_postfix}', f'tcp_{column_postfix}'], value_name=f'time_{value}')
     return melted_df
+
 
 def plot_boxplot(df, ax, x_data, y_data, title, xlabel, ylabel):
     """Plot a boxplot on the specified axes."""
@@ -21,18 +24,30 @@ def plot_boxplot(df, ax, x_data, y_data, title, xlabel, ylabel):
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
 
+
+def create_boxplots_for_each_value_of_independent_variable(df, metric):
+    for value in df[metric].unique():
+        # Filter the DataFrame based on the current unique value
+        filtered_df = df[df[metric] == value]
+
+        hs_df = combine_quic_and_tcp_values_for(filtered_df, 'hs', value)
+        conn_df = combine_quic_and_tcp_values_for(filtered_df, 'conn', value)
+
+        fig, axes = plt.subplots(2, 1, figsize=(8, 10))
+
+        plot_boxplot(hs_df, axes[0], 'mode', f'time_{value}',
+                     f'QUIC vs TCP Handshake | {metric} = {value}', 'Implementations', 'Time')
+        plot_boxplot(conn_df, axes[1], 'mode', f'time_{value}',
+                     f'QUIC vs TCP Connection | {metric} = {value}', 'Implementations', 'Time')
+
+        plt.tight_layout()
+        plt.savefig(f"{BOXPLOTS_DIR}/boxplots_{value}.png",
+                    dpi=300, bbox_inches='tight')
+        plt.show()
+
+
 def show_boxplot(df):
     """Create and display separate boxplots for handshake and connection times."""
-    hs_df = combine_quic_and_tcp_values_for(df, 'hs')
-    conn_df = combine_quic_and_tcp_values_for(df, 'conn')
-
-    fig, axes = plt.subplots(2, 1, figsize=(8, 10))
-
-    plot_boxplot(hs_df, axes[0], 'mode', 'time_hs', 'QUIC vs TCP Handshake', 'Implementations', 'Time')
-    plot_boxplot(conn_df, axes[1], 'mode', 'time_conn', 'QUIC vs TCP Connection', 'Implementations', 'Time')
-
-    plt.tight_layout()
-    plt.savefig(f"{BOXPLOTS_DIR}/boxplots.png", dpi=300, bbox_inches='tight')
-    plt.show()
-
-
+    test_case_settings = read_test_cases()
+    metric = find_keys_with_list_values(test_case_settings)
+    create_boxplots_for_each_value_of_independent_variable(df, metric)
