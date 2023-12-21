3b9fe903e5665b0a4dd2c3d6652da87b87906482
pullundertin <pullundertin@gmail.com>
2023-12-21 16:53:50 +0100

    conversion obj to dataframe and median_df

---
+++ modules/statistics.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+
+def get_medians(df):
+    median_df = pd.DataFrame()
+    group_columns = ['mode', 'size', 'delay', 'delay_deviation', 'loss',
+                     'rate', 'firewall', 'window_scaling', 'rmin', 'rmax', 'rdef', 'migration', 'generic_heatmap']
+    # Group by group_columns and calculate median across multiple columns
+    median_df = df.groupby(group_columns).agg({
+        'aioquic_hs': 'median',
+        'quicgo_hs': 'median',
+        'tcp_hs': 'median',
+        'aioquic_conn': 'median',
+        'quicgo_conn': 'median',
+        'tcp_conn': 'median'
+    }).reset_index()
+    return median_df
+
+
+def do_statistics(df):
+    median_df = get_medians(df)
+
+    return median_df
--- run.py
+++ run.py
@@ -8,8 +8,10 @@ from modules.heatmap import show_heatmaps
 from modules.boxplot import show_boxplot
 from modules.tests import run_tests
 from modules.converter import process_tcp_probe_logs
+from modules.statistics import do_statistics
 import os
 import argparse
+import pandas as pd
 
 
 def arguments():
@@ -44,10 +46,26 @@ def store_results(test_results, medians, args):
         rsync()
 
 
-def clean_dataframe(df):
-    df['quic_hs'] = df['aioquic_hs'].combine_first(df['quicgo_hs'])
-    df['quic_conn'] = df['aioquic_conn'].combine_first(df['quicgo_conn'])
-    return df
+# def clean_dataframe(df):
+#     df['quic_hs'] = df['aioquic_hs'].combine_first(df['quicgo_hs'])
+#     df['quic_conn'] = df['aioquic_conn'].combine_first(df['quicgo_conn'])
+#     return df
+
+def create_dataframe_from_object(test):
+    list_of_df = []
+
+    def convert_each_test_case_object_into_a_dataframe():
+        df = pd.DataFrame()
+        for test_case in test.test_cases_decompressed.test_cases:
+            df = pd.DataFrame([vars(test_case)])
+            list_of_df.append(df)
+
+    def add_each_dataframe_as_new_row_to_a_main_dataframe():
+        return pd.concat(list_of_df, axis=0)
+
+    convert_each_test_case_object_into_a_dataframe()
+    main_df = add_each_dataframe_as_new_row_to_a_main_dataframe()
+    return main_df
 
 
 if __name__ == "__main__":
@@ -68,8 +86,10 @@ if __name__ == "__main__":
         logging.info("Executing evaluation only")
 
     get_test_results(test)
-    print(test)
+    df = create_dataframe_from_object(test)
 
+    median_df = do_statistics(df)
+    print(median_df)
     # test_results = clean_dataframe(test_results)
     # evaluate_test_results(test_results, medians)
     # store_results(None, None, args)
