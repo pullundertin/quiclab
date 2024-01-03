b8dc3986e872bb65674cc71e547b1a98e475463f
pullundertin <pullundertin@gmail.com>
2024-01-03 15:21:35 +0100

    fixed issues with boxplot when controlparameter < 2

--- modules/boxplot.py
+++ modules/boxplot.py
@@ -37,8 +37,9 @@ def create_boxplots_for_each_single_stream(df, test):
     # Get unique combinations of 'mode' and 'control_parameter' from the 'test' object
     modes = df['mode'].unique()
     control_parameter = test.control_parameter
+    if not control_parameter:
+        return
     control_parameters = test.control_parameter_values
-    num_plots = len(modes) * len(control_parameters)
 
     fig, axes = plt.subplots(nrows=len(control_parameters), ncols=len(modes), figsize=(15, 6), sharey=True)
 
@@ -58,16 +59,27 @@ def create_boxplots_for_each_single_stream(df, test):
 
                 mode_control_df_melted = mode_control_df.melt(id_vars=['mode', control_parameter], value_vars=non_nan_columns, var_name='Stream_ID')
                 x_labels = [col.replace('_goodput', '') for col in sorted_cols]
-
-                sns.boxplot(data=mode_control_df_melted.dropna(), x='Stream_ID', y='value', ax=axes[j, i], palette='Set3')
-                axes[j, i].set_title(f'Mode: {mode}, Control Parameter: {control_param}')
-                axes[j, i].set_xlabel('Streams')
-                axes[j, i].set_ylabel('Goodput')
-                axes[j, i].set_xticks(range(len(non_nan_columns)))
-                axes[j, i].set_xticklabels(x_labels, rotation=90)
-                axes[j, i].legend().set_visible(False)
+                if len(control_parameters) < 2:
+                    sns.boxplot(data=mode_control_df_melted.dropna(), x='Stream_ID', y='value', ax=axes[i], palette='Set3')
+                    axes[i].set_title(f'Mode: {mode}, Control Parameter: {control_param}')
+                    axes[i].set_xlabel('Streams')
+                    axes[i].set_ylabel('Goodput')
+                    axes[i].set_xticks(range(len(non_nan_columns)))
+                    axes[i].set_xticklabels(x_labels, rotation=90)
+                    axes[i].legend().set_visible(False)
+                else:
+                    sns.boxplot(data=mode_control_df_melted.dropna(), x='Stream_ID', y='value', ax=axes[j, i], palette='Set3')
+                    axes[j, i].set_title(f'Mode: {mode}, Control Parameter: {control_param}')
+                    axes[j, i].set_xlabel('Streams')
+                    axes[j, i].set_ylabel('Goodput')
+                    axes[j, i].set_xticks(range(len(non_nan_columns)))
+                    axes[j, i].set_xticklabels(x_labels, rotation=90)
+                    axes[j, i].legend().set_visible(False)
             else:
-                axes[j, i].axis('off')  # Hide empty subplot if there are no non-null columns for the mode and control parameter
+                if len(control_parameters) < 2:
+                    axes[i].axis('off')  # Hide empty subplot if there are no non-null columns for the mode and control parameter
+                else:
+                    axes[j, i].axis('off')  # Hide empty subplot if there are no non-null columns for the mode and control parameter
 
     fig.suptitle('Goodput per Stream', y=1.05)
     plt.tight_layout()
