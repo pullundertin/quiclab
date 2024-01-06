a2efc4dfdf377a81a76286e45ce6db897a4bee49
pullundertin <pullundertin@gmail.com>
2024-01-06 19:49:20 +0100

    added pda to histogram

--- modules/histogram.py
+++ modules/histogram.py
@@ -2,6 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from modules.prerequisites import read_configuration
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns  # Import seaborn for KDE
 
 def show_histogram(df, control_parameter):
     HISTOGRAMS_DIR = read_configuration().get('HISTOGRAMS_DIR')
@@ -17,15 +19,20 @@ def show_histogram(df, control_parameter):
     for i, param in enumerate(parameters):
         for j, ctrl_param_value in enumerate(unique_control_parameter_values):
             grouped = df[(~df[param].isnull()) & (df[control_parameter] == ctrl_param_value)].groupby('mode')[param]
-            
+
             for mode, group in grouped:
-                axs[i, j].hist(group, bins='auto', alpha=0.5, rwidth=0.55, label=mode)
+                # Plot histogram
+                axs[i, j].hist(group, bins='auto', alpha=0.5, rwidth=0.55, label=mode, density=True)
+                # Plot KDE (PDA)
+                sns.kdeplot(group, ax=axs[i, j], label=f'{mode} KDE', linewidth=2)
+
             axs[i, j].set_title(f'Histogram for {param} - {control_parameter}: {ctrl_param_value}')
             axs[i, j].set_xlabel(param)
-            axs[i, j].set_ylabel('Frequency')
+            axs[i, j].set_ylabel('Density')
             # axs[i, j].text(23, 45, r'$\mu=15, b=3$')
             if param in ['goodput', 'quic_hs', 'quic_conn'] or param.startswith("Stream_ID_"):
                 axs[i, j].legend()
+
     plt.tight_layout()
     plt.savefig(f'{HISTOGRAMS_DIR}/histogram.png')
     plt.show()
