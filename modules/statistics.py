b1a8fa9a354a24b4e3ddee1314d4c453e759bbbc
pullundertin <pullundertin@gmail.com>
2024-01-18 17:13:54 +0100

    modified statistics.csv to show results grouped by mode and control parameter

--- modules/statistics.py
+++ modules/statistics.py
@@ -33,18 +33,26 @@ def get_medians(test_results_dataframe):
 
     return median_df
 
-def get_statistics_for_df(df):
+def get_statistics_for_df(df, control_parameter):
     TEST_RESULTS_DIR = read_configuration().get('TEST_RESULTS_DIR')
     TEST_RESULT_COLUMNS = read_configuration().get('TEST_RESULT_COLUMNS')
     stream_columns = [col for col in df.columns if col.startswith('Stream_ID_') and col.endswith('_goodput')]
     columns_to_print = TEST_RESULT_COLUMNS + stream_columns
-    filtered_df = df[columns_to_print].copy()
-    filtered_df.describe().to_csv(f'{TEST_RESULTS_DIR}/statistics.csv')
+    filtered_df = df.copy()
+    # Group by 'mode' and calculate statistics
+    grouped_statistics = filtered_df.groupby(['mode', control_parameter])
+    grouped_statistics = grouped_statistics[columns_to_print].describe()
+    grouped_statistics = grouped_statistics.round(4)
 
-def do_statistics(df):
+    # Save grouped statistics to CSV
+    grouped_statistics.to_csv(f'{TEST_RESULTS_DIR}/statistics.csv')
+
+
+def do_statistics(df, test):
+    control_parameter = test.control_parameter
     update_program_progress_bar('Do Statistics')
    
-    get_statistics_for_df(df)
+    get_statistics_for_df(df, control_parameter)
     median_df = get_medians(df)
 
     return median_df
