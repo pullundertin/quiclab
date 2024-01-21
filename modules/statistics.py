44a41531913b4174401d81a2763b9d082bbf35cb
pullundertin <pullundertin@gmail.com>
2024-01-21 11:17:49 +0100

    rebuilt statistics module

--- modules/statistics.py
+++ modules/statistics.py
@@ -5,6 +5,7 @@ from modules.prerequisites import read_configuration
 
 TEST_CONFIG_COLUMNS = read_configuration().get("TEST_CONFIG_COLUMNS")
 TEST_RESULT_COLUMNS = read_configuration().get("TEST_RESULT_COLUMNS")
+TEST_RESULTS_DIR = read_configuration().get("TEST_RESULTS_DIR")
 
 
 def get_medians(test_results_dataframe):
@@ -39,9 +40,78 @@ def get_medians(test_results_dataframe):
     return median_df
 
 
+def filter_columns_to_calculate_statistics_on(df, control_parameter):
+    stream_columns = [col for col in df.columns if col.startswith('Stream_')]
+    quic_columns = [col for col in df.columns if col.startswith('quic_')]
+    # Exclude 'mode' and control_parameter columns from statistics
+    columns_to_exclude = TEST_CONFIG_COLUMNS + \
+        [control_parameter] + stream_columns + quic_columns
+    columns_to_calculate_statistics_on = [
+        col for col in df.columns if col not in columns_to_exclude]
+
+    return columns_to_calculate_statistics_on
+
+
+def generate_statistics_on_test_results(df, control_parameter):
+
+    columns_to_calculate_statistics_on = filter_columns_to_calculate_statistics_on(
+        df, control_parameter)
+
+    # Group by 'mode' and control_parameter, then describe the selected columns
+    grouped_statistics = df.groupby(['mode', control_parameter])[
+        columns_to_calculate_statistics_on].describe()
+
+    # Flatten multi-level columns to a single level
+    grouped_statistics.columns = [
+        '_'.join(col).strip() for col in grouped_statistics.columns.values]
+
+    return grouped_statistics
+
+
+def merge_columns_with_the_same_metric(df):
+    modes = ['aioquic_', 'quicgo_', 'tcp_']
+    result_dict = {}
+
+    for mode in modes:
+        # Get column names starting with the current mode
+        filtered_columns = [col for col in df.columns if col.startswith(mode)]
+
+        # Extract the part of the column names that comes after the mode
+        result_dict[mode] = [col[len(mode):] for col in filtered_columns]
+
+    # Flatten the lists in result_dict.values() and convert to a set to get unique values
+    unique_values = set(val for sublist in result_dict.values()
+                        for val in sublist)
+    columns_to_drop = []
+    for value in unique_values:
+        columns_to_combine = []
+        for key in result_dict.keys():
+            columns_to_combine.append(f'{key}{value}')
+        df[f'{value}'] = df[columns_to_combine].replace(
+            'None', '').sum(1)
+        columns_to_drop.append(columns_to_combine)
+
+    # Flatten the list of lists
+    columns_to_drop_flat = [
+        col for sublist in columns_to_drop for col in sublist]
+
+    # Drop the original columns
+    df = df.drop(columns=columns_to_drop_flat)
+    return df
+
+
+def calculate_relationships_for_each_column(merged_df):
+    for col in merged_df.columns:
+        # Create a new column for each column with the ratio between each value of index control_parameter
+        # with different values for index 'mode'
+        merged_df[f'{col}_tcp_ratio'] = (merged_df[col] /
+                                         merged_df.loc['tcp', col] * 100) - 100
+    return merged_df
+
+
 def sort_statistics(df):
-    metrics_order = ['goodput', 'aioquic_hs', 'aioquic_conn',
-                     'quicgo_hs', 'quicgo_conn', 'tcp_hs', 'tcp_conn']
+    metrics_order = ['goodput', 'hs', 'conn',
+                     'link_utilization', 'jfi']
 
     statistics_order = ['mean', 'mean_tcp_ratio',
                         'std', 'min', '25%', '50%', '75%', 'max']
@@ -68,99 +138,21 @@ def sort_statistics(df):
     return sorted_df
 
 
-def sort_merged_df(df):
-    # Get the columns ending with '_tcp_ratio'
-    tcp_ratio_columns = [
-        col for col in df.columns if col.endswith('_tcp_ratio')]
-
-    # Create a list of tuples where each tuple contains the column without '_tcp_ratio' and its corresponding column with '_tcp_ratio'
-    sorted_columns = [(col.replace('_tcp_ratio', ''), col)
-                      for col in tcp_ratio_columns]
-
-    # Flatten the list of tuples
-    sorted_columns_flat = [col for pair in sorted_columns for col in pair]
-
-    # Select the sorted columns and other remaining columns
-    sorted_df = df[sorted_columns_flat +
-                   [col for col in df.columns if col not in sorted_columns_flat]]
-
-    return sorted_df
-
-
-def merge_columns_with_the_same_stat(df):
-    modes = ['aioquic_', 'quicgo_', 'tcp_']
-
-    # Get column names starting with elements from the list
-    filtered_columns = [col for col in df.columns if any(
-        col.startswith(mode) for mode in modes)]
-
-    # Initialize an empty dictionary to store results
-    result_dict = {}
-
-    # Iterate through each mode
-    for mode in modes:
-        # Get column names starting with the current mode
-        filtered_columns = [col for col in df.columns if col.startswith(mode)]
-
-        # Extract the part of the column names that comes after the mode
-        result_dict[mode] = [col[len(mode):] for col in filtered_columns]
-
-    # Flatten the lists in result_dict.values() and convert to a set to get unique values
-    unique_values = set(val for sublist in result_dict.values()
-                        for val in sublist)
-    columns_to_drop = []
-    for value in unique_values:
-        columns_to_combine = []
-        for key in result_dict.keys():
-            columns_to_combine.append(f'{key}{value}')
-        df[f'{value}'] = df[columns_to_combine].replace(
-            'None', '').sum(1)
-        columns_to_drop.append(columns_to_combine)
-
-    # Flatten the list of lists
-    columns_to_drop_flat = [
-        col for sublist in columns_to_drop for col in sublist]
-
-    # Drop the specified columns
-    df = df.drop(columns=columns_to_drop_flat)
-    return df
-
-
 def get_statistics(df, control_parameter):
 
-    TEST_RESULTS_DIR = read_configuration().get('TEST_RESULTS_DIR')
-
-    # Exclude 'mode' and control_parameter columns from statistics
-    columns_to_exclude = ['mode', control_parameter]
-    columns_to_calculate_statistics_on = [
-        col for col in df.columns if col not in columns_to_exclude]
-
-    # Group by 'mode' and control_parameter, then describe the selected columns
-    grouped_statistics = df.groupby(['mode', control_parameter])[
-        columns_to_calculate_statistics_on].describe()
-
-    # Flatten multi-level columns to a single level
-    grouped_statistics.columns = [
-        '_'.join(col).strip() for col in grouped_statistics.columns.values]
-
-    grouped_statistics = sort_statistics(grouped_statistics).round(4)
-
-    merged_df = merge_columns_with_the_same_stat(grouped_statistics)
-    # Calculate relationships for each column
-    for col in merged_df.columns:
-        # Create a new column for each column with the ratio between each value of index control_parameter
-        # with different values for index 'mode'
-        merged_df[f'{col}_tcp_ratio'] = (merged_df[col] /
-                                         merged_df.loc['tcp', col] * 100) - 100
-    sorted_merged_df = sort_merged_df(merged_df)
-    sorted_merged_df.to_csv(f'{TEST_RESULTS_DIR}/statistics.csv')
+    statistics = generate_statistics_on_test_results(df, control_parameter)
+    merged_df = merge_columns_with_the_same_metric(statistics)
+    relationships_df = calculate_relationships_for_each_column(merged_df)
+    sorted_df = sort_statistics(relationships_df)
+    results_df = sorted_df.round(4)
+    results_df.to_csv(f'{TEST_RESULTS_DIR}/statistics.csv')
 
 
 def do_statistics(df, test):
     control_parameter = test.control_parameter
     update_program_progress_bar('Do Statistics')
-
-    get_statistics(df, control_parameter)
+    tmp_df = df.copy()
+    get_statistics(tmp_df, control_parameter)
     median_df = get_medians(df)
 
     return median_df
