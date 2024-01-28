3f0a9dfdeb3f56e80fe2573837ecf20e4a074ecd
pullundertin <pullundertin@gmail.com>
2024-01-28 08:57:39 +0000

    changed conditions for arguments in run

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from modules.commands import rsync_permanent
 from modules.logs import log_config
 from modules.prerequisites import get_test_object, arguments, check_if_folders_for_results_exist, delete_old_test_results
 from modules.program_steps import generate_new_data, extract_data, read_data, evaluate_data, visualize_data
-
+from modules.statistics import do_statistics
 
 def main():
 
@@ -13,6 +13,7 @@ def main():
     median_dataframe = None
 
     args = arguments()
+    print(args)
     log_config(args)
 
     test = get_test_object(args)
@@ -22,13 +23,14 @@ def main():
     if args.test:
         logging.info(f"{os.getenv('HOST')}: full execution enabled")
         generate_new_data(test)
-    elif args.extract:
+    if args.extract:
         logging.info("Executing without running tests")
         extract_data(test)
     else:
         logging.info("Reading data from files")
         test_results_dataframe, median_dataframe = read_data()
 
+    # do_statistics(test_results_dataframe, test)
     if args.evaluate:
         evaluate_data(test_results_dataframe, test)
     if args.viz:
